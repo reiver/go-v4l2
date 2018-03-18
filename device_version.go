4b5e938b684ac2b769c93450bf80db8fe2298330
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-03-17 20:32:27 -0700

    version

---
+++ device_version.go
@@ -0,0 +1,29 @@
+package v4l2
+
+// Version returns the version number of the driver.
+//
+// Example values that Version might return are:
+//
+// • "4.4.98"
+//
+// • "4.14.0"
+func (receiver Device) Version() (string, error) {
+	if !receiver.opened {
+		return "", errNotOpen
+	}
+	if !receiver.capQueried {
+		return "", errInternalError
+	}
+
+	return receiver.cap.Version(), nil
+}
+
+// MustVersion is like Version, except it panic()s if there is an error.
+func (receiver Device) MustVersion() string {
+	datum, err := receiver.Version()
+	if nil != err {
+		panic(err)
+	}
+
+	return datum
+}
