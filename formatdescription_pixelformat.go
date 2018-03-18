a2e22d524789bc6f6e1383ca4d6bb4a8b965020b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-03-18 03:52:03 -0700

    minor fourcc refactor

--- formatdescription_pixelformat.go
+++ formatdescription_pixelformat.go
@@ -1,9 +1,5 @@
 package v4l2
 
-import (
-	"fmt"
-)
-
 // PixelFormat returns the pixel format as a FOURCC (4CC) code.
 //
 // Some examples of what might be returned by PixelFormat are;
@@ -14,10 +10,5 @@ import (
 //
 // • etc,
 func (receiver FormatDescription) PixelFormat() string {
-	a := byte( receiver.internal.pixelFormat        & 0xff)
-	b := byte((receiver.internal.pixelFormat >>  8) & 0xff)
-	c := byte((receiver.internal.pixelFormat >> 16) & 0xff)
-	d := byte((receiver.internal.pixelFormat >> 24) & 0xff)
-
-	return fmt.Sprintf("%c%c%c%c", a, b, c, d)
+	return fourcc(receiver.internal.pixelFormat).String()
 }
