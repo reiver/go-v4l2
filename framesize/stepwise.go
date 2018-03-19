6a43f07139aa8d1f29082fb9bc118ee6b405d8c7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-03-19 01:06:26 -0700

    v4l2_framesize

---
+++ framesize/stepwise.go
@@ -0,0 +1,21 @@
+package v4l2_framesize
+
+import (
+	"github.com/reiver/go-v4l2/pixelformat"
+)
+
+type Stepwise struct {
+	index              uint32                // Frame size number
+	PixelFormat        v4l2_pixelformat.Type // Pixel format
+	typ                uint32                // Frame size type the device supports.
+
+	MinWidth           uint32                // Minimum frame width [pixel]
+	MaxWidth           uint32                // Maximum frame width [pixel]
+	StepWidth          uint32                // Frame width step size [pixel]
+	MinHeight          uint32                // Minimum frame height [pixel]
+	MaxHeight          uint32                // Maximum frame height [pixel]
+	StepHeight         uint32                // Frame height step size [pixel]
+
+	reserved        [2]uint32                // Reserved space for future use
+}
+
