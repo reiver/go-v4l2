4b105aa32aa64c875ddaab0abb4cba7d98fd6f86
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-03-19 01:20:28 -0700

    v4l2_framesize

--- framesize/cast.go
+++ framesize/cast.go
@@ -11,6 +11,23 @@ const (
 	const_TYPE_STEPWISE   = 3 // (V4L2_FRMSIZE_TYPE_STEPWISE)
 )
 
+// Cast tries to turn a v4l2_framesize.Type into a:
+// v4l2_framesize.Discrete,
+// v4l2_framesize.Continuous, or
+// v4l2_framesize.Stepwise.
+//
+// Example:
+//
+//	switch casted := frameSize.Cast().(type) {
+//	case v4l2_framesize.Continuous
+//		//@TODO
+//	case v4l2_framesize.Discrete:
+//		//@TODO
+//	case v4l2_framesize.Stepwise
+//		//@TODO
+//	default:
+//		//@TODO: Error, unknown framesize type.
+//	}
 func (receiver Type) Cast() (interface{}, error) {
 	switch receiver.typ {
 	case const_TYPE_DISCRETE:
